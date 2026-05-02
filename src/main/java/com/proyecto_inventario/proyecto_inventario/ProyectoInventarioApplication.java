986568574b084ff7e1ba345d6201b9a63362034d
Marcelo Loyola <ma.loyolah@duocuc.cl>
2026-05-02 09:41:51 -0400

    listo

---
+++ src/main/java/com/proyecto_inventario/proyecto_inventario/ProyectoInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_inventario.proyecto_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInventarioApplication.class, args);
+	}
+
+}
