3d16e86ac4a8bbe2a6bc7a0a912613c2af4d210b
Shany-web <dashcarceneus@gmail.com>
2026-05-07 00:07:55 -0400

    Creacion de ubicación service.

---
+++ src/main/java/com/proyecto_inventario/proyecto_inventario/service/UbicacionService.java
@@ -0,0 +1,61 @@
+package com.proyecto_inventario.proyecto_inventario.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto_inventario.proyecto_inventario.model.Ubicacion;
+import com.proyecto_inventario.proyecto_inventario.repository.UbiacionRepository;
+
+@Service
+public class UbicacionService {
+    @Autowired
+    private UbiacionRepository repository;
+
+    public Ubicacion buscarPorId(Integer id) {
+        Ubicacion ubicacion = repository.findUbicacionById(id);
+
+        return ubicacion;
+    }
+
+    public List<Ubicacion> ubicaciones() {
+        return repository.findAll();
+    }
+
+    public ArrayList<Ubicacion> buscarPorPasillo(Integer pasillo) {
+        return repository.findByPasillo(pasillo);
+
+    }
+
+    public ArrayList<Ubicacion> buscarPorDescripcion(String descripcion) {
+        return repository.findByDescripcionAllIgnoreCase(descripcion);
+    }
+
+    public ArrayList<Ubicacion> buscarPorEstante(Integer estante) {
+        return repository.findByEstante(estante);
+    }
+
+    public void eliminarPorId(Integer id) {
+        repository.deleteById(id);
+
+    }
+
+    public Ubicacion guardarUbicacion(Ubicacion ubi) {
+        return repository.save(ubi);
+    }
+
+    public void actualizarUbicacion(Integer id, Ubicacion ubi) {
+        Ubicacion ubica = repository.findById(id).orElse(null);
+
+        if (ubica != null) {
+            ubica.setDescripcion(ubi.getDescripcion());
+            ubica.setEstante(ubi.getEstante());
+            ubica.setPasillo(ubi.getPasillo());
+            ubica.setDescripcion(ubi.getDescripcion());
+
+            repository.save(ubica);
+        }
+    }
+
+}
