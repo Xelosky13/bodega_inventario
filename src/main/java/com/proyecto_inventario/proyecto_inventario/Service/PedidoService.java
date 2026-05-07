f0464e2729c77b1c0179b5cbb5b1bd2e015b2e19
Xelosky13 <ma.loyolah@duocuc.cl>
2026-05-07 01:06:50 -0400

    Actualizando el nombre de la clase ClienteService

--- src/main/java/com/proyecto_inventario/proyecto_inventario/Service/PedidoService.java
+++ src/main/java/com/proyecto_inventario/proyecto_inventario/Service/PedidoService.java
@@ -19,7 +19,7 @@ public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
 
-    private PedidoDTO convertirDTO(Pedido pedido){
+    public PedidoDTO convertirADTO(Pedido pedido){
         PedidoDTO dto = new PedidoDTO();
         dto.setId(pedido.getId());
         dto.setEstado(pedido.getEstado());
@@ -31,9 +31,57 @@ public class PedidoService {
             itemDto.setCantidad(item.getCantidad());
             return itemDto;
         }).toList();
-
+        dto.setItems(itemsDTOs);
+        return dto;
     }
-    
 
+    public List<PedidoDTO> obtenerTodos(){
+        return pedidoRepository.findAll().stream()
+                .map(this::convertirADTO)
+                .toList();
+    }
 
+    public PedidoDTO buscarPorId(Integer id){
+        Pedido pedido = pedidoRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Pedido no encontrado"));
+        return convertirADTO(pedido);
+    }
+
+    public String eliminar(Integer id){
+        try {
+            Pedido pedido = pedidoRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("No se puede eliminar, el pedido "+ id + "eliminado"));
+            pedidoRepository.delete(pedido);
+            return "El pedido " + pedido.getId() + "ha sido eliminado";
+        } catch (RuntimeException e){
+            return e.getMessage();
+        }
+    }
+
+    public Pedido guardarPedido(Pedido pedido){
+        return pedidoRepository.save(pedido);
+    }
+
+    public Pedido actualizarPedido(Integer id,Pedido pedido){
+        Pedido pedido2 = pedidoRepository.findById(id).orElseThrow(() -> new RuntimeException("Pedido no existe"));
+        if(pedido.getCliente() != null){
+            pedido2.setCliente(pedido.getCliente()); 
+        }
+        if(pedido.getEstado() != null){
+            pedido2.setEstado(pedido.getEstado());
+        }
+        if(pedido.getFechaPedido() != null){
+            pedido2.setFechaPedido(pedido.getFechaPedido());
+        }
+        if(pedido.getId() != null){
+            pedido2.setItems(pedido.getItems());
+        }
+        return pedidoRepository.save(pedido);
+    }
+
+    public List<PedidoDTO> buscarPendientesPorCliente(String rut){
+        return pedidoRepository.buscarPendientesPorCliente(rut).stream()
+            .map(this :: convertirADTO)
+            .toList();
+    }
 }
