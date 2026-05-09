53aecc7ad6be038f56fb158c7c8e7423a72d71d1
Xelosky13 <ma.loyolah@duocuc.cl>
2026-05-09 11:57:52 -0400

    final marcelo

--- src/main/java/com/proyecto_inventario/proyecto_inventario/controller/ClienteController.java
+++ src/main/java/com/proyecto_inventario/proyecto_inventario/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.proyecto_inventario.proyecto_inventario.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
